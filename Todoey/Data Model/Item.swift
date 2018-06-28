0c8301b81904386f12a8ba62fb6bfcdcb4ee0f14
Tyler von Burg <tylervonburg@tylers-macbook-air.local>
2018-06-28 11:25:54 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Tyler von Burg on 6/25/18.
+//  Copyright © 2018 Tyler von Burg. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
